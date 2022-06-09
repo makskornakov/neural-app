fc4ffbdbbd832684dc9adebfe8933f7c7cb1e718
Maksim Kornakov <makskornakov@ya.ru>
2022-06-09 06:10:33 +0200

    Update Training.tsx

--- src/components/Training.tsx
+++ src/components/Training.tsx
@@ -111,8 +111,8 @@ function Training() {
           drawingFunc={(localCtx) => localCtx.ellipse(200, 200, 100, 100, 0, 0, 2 * Math.PI)}
         />
         <ButtonToDrawShape shapeName="cross" drawingFunc={(localCtx) => {
-          localCtx.rect(175, 50, 50, 300);
-          localCtx.rect(50, 175, 300, 50);
+          localCtx.rect(165, 50, 70, 300);
+          localCtx.rect(50, 165, 300, 70);
         }}/>
         <ButtonToDrawShape shapeName="triangle" drawingFunc={(localCtx) => {
           localCtx.moveTo(200, 100);
